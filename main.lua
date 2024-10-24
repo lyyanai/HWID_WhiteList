e442d7a0b541e0625e6d95891907000e5515c9d9
nexer1234 <77268699+Pro666Pro@users.noreply.github.com>
2024-10-24 10:45:47 +0900

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,4 +1,3 @@
-game:GetService("StarterGui"):SetCore("SendNotification",{Title = "Checking...",Text = "Checking User HWID..." ,Duration = 10, Icon = "rbxthumb://type=Asset&id=9649923610&w=150&h=150",Button1 = "ok"})
 return {
 "69057e34-39da-4846-85ff-af47755e2be7",
 "amogus" -- HWID Whitelist System
