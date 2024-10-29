a5bfefd279868c9179dba21067e2578f98b3a309
nexer1234 <77268699+Pro666Pro@users.noreply.github.com>
2024-10-29 20:20:16 +0900

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,13 +1,7 @@
 return {
   "69057e34-39da-4846-85ff-af47755e2be7", -- nexer1234 AKA Nexer1234 HWID
   "d7fa905a-c32b-4943-b795-f2a79d284bb6", -- legendlife2160 AKA Simple_Guy HWID
-  "d7fa905a-c32b-4943-b795-f2a79d284bb6", -- legendlife2160 AKA Simple_Guy HWID
-  "d7fa905a-c32b-4943-b795-f2a79d284bb6", -- legendlife2160 AKA Simple_Guy HWID
-  "d7fa905a-c32b-4943-b795-f2a79d284bb6", -- legendlife2160 AKA Simple_Guy HWID
-  "d7fa905a-c32b-4943-b795-f2a79d284bb6", -- legendlife2160 AKA Simple_Guy HWID
-  "d7fa905a-c32b-4943-b795-f2a79d284bb6", -- legendlife2160 AKA Simple_Guy HWID
-  "d7fa905a-c32b-4943-b795-f2a79d284bb6", -- legendlife2160 AKA Simple_Guy HWID
-  "d7fa905a-c32b-4943-b795-f2a79d284bb6", -- legendlife2160 AKA Simple_Guy HWID
-  "d7fa905a-c32b-4943-b795-f2a79d284bb6", -- legendlife2160 AKA Simple_Guy HWID
+  "a657d5ad-1f73-47a0-90d7-d359d4c3fe6a", -- nexer1234 AKA Nexer1234 HWID { New }
+  "skibidi" -- kys - keep yourself safe
 }
 -- what are you doing here lil bro
