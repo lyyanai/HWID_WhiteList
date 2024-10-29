c571efc227233a0943f088a5714ba933785e6e9c
nexer1234 <77268699+Pro666Pro@users.noreply.github.com>
2024-10-29 15:28:19 +0900

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,6 +1,13 @@
 return {
-"69057e34-39da-4846-85ff-af47755e2be7", -- nexer1234 AKA Nexer1234 HWID
-"d7fa905a-c32b-4943-b795-f2a79d284bb6", -- legendlife2160 AKA Simple_Guy HWID
-"Write HWID Here" -- HWID Whitelist System
+  "69057e34-39da-4846-85ff-af47755e2be7", -- nexer1234 AKA Nexer1234 HWID
+  "d7fa905a-c32b-4943-b795-f2a79d284bb6", -- legendlife2160 AKA Simple_Guy HWID
+  "d7fa905a-c32b-4943-b795-f2a79d284bb6", -- legendlife2160 AKA Simple_Guy HWID
+  "d7fa905a-c32b-4943-b795-f2a79d284bb6", -- legendlife2160 AKA Simple_Guy HWID
+  "d7fa905a-c32b-4943-b795-f2a79d284bb6", -- legendlife2160 AKA Simple_Guy HWID
+  "d7fa905a-c32b-4943-b795-f2a79d284bb6", -- legendlife2160 AKA Simple_Guy HWID
+  "d7fa905a-c32b-4943-b795-f2a79d284bb6", -- legendlife2160 AKA Simple_Guy HWID
+  "d7fa905a-c32b-4943-b795-f2a79d284bb6", -- legendlife2160 AKA Simple_Guy HWID
+  "d7fa905a-c32b-4943-b795-f2a79d284bb6", -- legendlife2160 AKA Simple_Guy HWID
+  "d7fa905a-c32b-4943-b795-f2a79d284bb6", -- legendlife2160 AKA Simple_Guy HWID
 }
 -- what are you doing here lil bro
