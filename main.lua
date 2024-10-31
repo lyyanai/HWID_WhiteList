08d2e9c30627451443518eb79f327f7ae4a8920e
nexer1234 <77268699+Pro666Pro@users.noreply.github.com>
2024-11-01 01:18:19 +0900

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,8 +1,9 @@
 return {
-  "69057e34-39da-4846-85ff-af47755e2be7", -- nexer1234 AKA Nexer1234 HWID
+  "69057e34-39da-4846-85ff-af47755e2be7", -- nexer1234 AKA Nexer1234 HWID { Old }
   "d7fa905a-c32b-4943-b795-f2a79d284bb6", -- legendlife2160 AKA Simple_Guy HWID
   "a657d5ad-1f73-47a0-90d7-d359d4c3fe6a", -- nexer1234 AKA Nexer1234 HWID { New }
   "f223f01b-7f5a-4729-a8fc-3e8d007660ff", -- noob365 AKA Nameless Bot HWID
+  "b9cd528f-6948-416f-bd57-168828a39ea1", -- friend11buuguu19777 AKA notcool HWID
   "skibidi" -- kys - keep yourself safe
 }
 -- what are you doing here lil bro
