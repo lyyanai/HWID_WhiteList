cebf4d6e9ded80fa8cce2c165cdcbc4b59f7914a
nexer1234 <77268699+Pro666Pro@users.noreply.github.com>
2024-11-01 21:35:27 +0900

    Update main.lua

--- main.lua
+++ main.lua
@@ -5,6 +5,7 @@ return {
   "f223f01b-7f5a-4729-a8fc-3e8d007660ff", -- noob365 AKA Nameless Bot HWID
   "b9cd528f-6948-416f-bd57-168828a39ea1", -- friend11buuguu19777 AKA notcool HWID
   "0661eed7-d0d1-4331-ae4a-41f3a369869a", -- randomxd01090 AKA mr bacon HWID
+  "f2be4a41-0735-40af-8c48-a7eeece7035a", -- niceon1 AKA hehehehehehehehe HWID
   "skibidi" -- kys - keep yourself safe
 }
 -- what are you doing here lil bro
