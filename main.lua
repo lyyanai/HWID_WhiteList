edb0058f93031faddb8412f912275ffe44ccae61
nexer1234 <77268699+Pro666Pro@users.noreply.github.com>
2024-11-02 13:57:21 +0900

    Update main.lua

--- main.lua
+++ main.lua
@@ -6,6 +6,7 @@ return {
   "b9cd528f-6948-416f-bd57-168828a39ea1", -- friend11buuguu19777 AKA notcool HWID
   "0661eed7-d0d1-4331-ae4a-41f3a369869a", -- randomxd01090 AKA mr bacon HWID
   "f2be4a41-0735-40af-8c48-a7eeece7035a", -- niceon1 AKA hehehehehehehehe HWID
+  "7a8f2ab5-57c1-40f8-9aed-4ff5d794af29", -- rom6481 AKA idk HWID
   "skibidi" -- kys - keep yourself safe
 }
 -- what are you doing here lil bro
