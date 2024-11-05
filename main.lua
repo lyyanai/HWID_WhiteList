c8e50afa4bc141044181ea330fd63915f328674d
nexer1234 <77268699+Pro666Pro@users.noreply.github.com>
2024-11-05 13:30:18 +0900

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,7 +1,7 @@
 return {
   "69057e34-39da-4846-85ff-af47755e2be7", -- nexer1234 AKA Nexer1234 HWID { Old }
   "d7fa905a-c32b-4943-b795-f2a79d284bb6", -- legendlife2160 AKA Simple_Guy HWID
-  "a657d5ad-1f73-47a0-90d7-d359d4c3fe6a", -- nexer1234 AKA Nexer1234 HWID { New }
+  "a657d5ad-1f73-47a0-90d7-d359d4c3fe6a", -- nexer1234 AKA Nexer1234 HWID { Old }
   "f223f01b-7f5a-4729-a8fc-3e8d007660ff", -- noob365 AKA Nameless Bot HWID
   "b9cd528f-6948-416f-bd57-168828a39ea1", -- friend11buuguu19777 AKA notcool HWID
   "0661eed7-d0d1-4331-ae4a-41f3a369869a", -- randomxd01090 AKA mr bacon HWID
@@ -12,6 +12,7 @@ return {
   "399341a5-ae1c-48d4-8276-6420bbaa7103", -- niceon1 AKA hehehehehehehehe HWID { New }
   "4BEB2D40-00AF-46DD-955A-0D3153C91A78", -- stas0354 AKA Scripter HWID #1
   "4beb2d40-00af-46dd-955a-0d3153c91a78", -- stas0354 AKA Scripter HWID #2
+  "203b0997-d374-474e-989c-7c8aa6607ad5", -- nexer1234 AKA Nexer1234 HWID { New }
   "skibidi" -- kys - keep yourself safe
 }
 -- what are you doing here lil bro
