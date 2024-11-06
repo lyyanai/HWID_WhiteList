209cc583a93eaa1a2a0b0589c40dcccc77c64bd2
nexer1234 <77268699+Pro666Pro@users.noreply.github.com>
2024-11-06 19:49:11 +0900

    Update main.lua

--- main.lua
+++ main.lua
@@ -3,17 +3,19 @@ return {
   "f223f01b-7f5a-4729-a8fc-3e8d007660ff", -- noob365 AKA Nameless Bot HWID
   "b9cd528f-6948-416f-bd57-168828a39ea1", -- friend11buuguu19777 AKA notcool HWID
   "0661eed7-d0d1-4331-ae4a-41f3a369869a", -- randomxd01090 AKA mr bacon HWID
-  "1D0C9AED-5B94-42DB-9DF0-BBF13B2A3403", -- rom6481 AKA idk HWID #1
-  "1d0c9aed-5b94-42db-9df0-bbf13b2a3403", -- rom6481 AKA idk HWID #2
+  "1D0C9AED-5B94-42DB-9DF0-BBF13B2A3403", -- rom6481 AKA idk HWID { PC }
+  "1d0c9aed-5b94-42db-9df0-bbf13b2a3403", -- rom6481 AKA idk HWID { PC }
   "28f94b21-dfe9-4aee-ad78-b70d76623a94", -- barond._ AKA Yhazhy HWID
   "399341a5-ae1c-48d4-8276-6420bbaa7103", -- niceon1 AKA hehehehehehehehe HWID
-  "4BEB2D40-00AF-46DD-955A-0D3153C91A78", -- stas0354 AKA Scripter HWID #1
-  "4beb2d40-00af-46dd-955a-0d3153c91a78", -- stas0354 AKA Scripter HWID #2
+  "4BEB2D40-00AF-46DD-955A-0D3153C91A78", -- stas0354 AKA Scripter HWID { PC } 
+  "4beb2d40-00af-46dd-955a-0d3153c91a78", -- stas0354 AKA Scripter HWID { PC }
   "203b0997-d374-474e-989c-7c8aa6607ad5", -- nexer1234 AKA Nexer1234 HWID
   "868a29ed-d2c7-476d-baa8-78ad2bcbff16", -- yesbro0019 AKA Yes Bro HWID
   "4bc38f00-acec-4f1e-9b88-06b5ce89521e", -- mdmn_. AKA dkdk HWID
   "4E9E6144-D779-4329-97F8-F4A193C83495", -- nexer1234 AKA Nexer1234 HWID { PC }
-  "4e9e6144-d779-4329-97f8-f4a193c83495",-- nexer1234 AKA Nexer1234 HWID { PC }
+  "4e9e6144-d779-4329-97f8-f4a193c83495", -- nexer1234 AKA Nexer1234 HWID { PC }
+  "759864FE-D9EE-441F-94DD-20E3E12EF596", -- thewho0404 AKA thewho HWID { PC }
+  "759864fe-d9ee-441f-94dd-20e3e12ef596", -- thewho0404 AKA thewho HWID { PC }
   "skibidi" -- kys - keep yourself safe
 }
 -- what are you doing here lil bro
