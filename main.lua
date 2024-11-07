05421bdee9612494aa62d1df78800e0072ad1f55
nexer1234 <77268699+Pro666Pro@users.noreply.github.com>
2024-11-07 23:04:16 +0900

    Update main.lua

--- main.lua
+++ main.lua
@@ -17,6 +17,7 @@ return {
   "759864FE-D9EE-441F-94DD-20E3E12EF596", -- thewho0404 AKA thewho HWID { PC }
   "759864fe-d9ee-441f-94dd-20e3e12ef596", -- thewho0404 AKA thewho HWID { PC }
   "51ade8fd-a7f3-4e8d-a6a3-e3983ee54103", -- thewho0404 AKA thewho HWID
+  "85fe8ed6-4020-43f4-8479-2dab1e8a4ab7", -- noob12345678999 AKA demon HWID
   "skibidi" -- kys - keep yourself safe
 }
 -- what are you doing here lil bro
