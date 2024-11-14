a2b503f8c62625fe7093e65c06a6ed9af87c66d9
nexer1234 <77268699+Pro666Pro@users.noreply.github.com>
2024-11-14 12:18:16 +0900

    Update main.lua

--- main.lua
+++ main.lua
@@ -6,7 +6,7 @@ return {
   "1D0C9AED-5B94-42DB-9DF0-BBF13B2A3403", -- rom6481 AKA idk HWID { PC }
   "1d0c9aed-5b94-42db-9df0-bbf13b2a3403", -- rom6481 AKA idk HWID { PC }
   "136a3ed9-8ef8-461c-9628-84421a622642", -- barond._ AKA Yhazhy HWID
-  "d2402829-6945-4ce8-a397-4ec7baaa09a0", -- niceon1 AKA hehehehehehehehe HWID
+  "babd79dc-ce55-4d31-bcf9-1f72ccd30060", -- niceon1 AKA hehehehehehehehe HWID
   "4BEB2D40-00AF-46DD-955A-0D3153C91A78", -- stas0354 AKA Scripter HWID { PC } 
   "4beb2d40-00af-46dd-955a-0d3153c91a78", -- stas0354 AKA Scripter HWID { PC }
   "203b0997-d374-474e-989c-7c8aa6607ad5", -- nexer1234 AKA Nexer1234 HWID
@@ -31,6 +31,7 @@ return {
   "665306cb-67c8-4ba4-b3d5-bfd749bf1e0c", -- nadzil HWID
   "FF272E35-34C1-43EB-B7B7-62992D0DE761", -- on sigma hwid
   "dc121f00-c103-4f4b-ab7f-e08cf280f240", -- stardeww_ AKA starzxx HWID
+  "a4de64d9-29a8-4888-b853-bf74b35b90f9", -- strikerpro AKA strikerpro_gamer HWID
   "skibidi" -- kys - keep yourself safe
 }
 -- what are you doing here lil bro
