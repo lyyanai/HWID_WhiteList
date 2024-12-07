81ff14830a67f1f8c38a911f169ca41130632a9e
nexer1234 <77268699+Pro666Pro@users.noreply.github.com>
2024-12-08 03:18:27 +0900

    Update main.lua

--- main.lua
+++ main.lua
@@ -84,6 +84,7 @@ return {
   "3250e112-046a-4a81-a4f8-8f3a62bb3fdf", -- ellierrrr. hwid
   "aa8ff940-cfd3-4f33-8167-198dbad1f5ab",--uchiha_sasuk_e
   "5FFCBF1C-2A4F-418C-97FD-7405E09465A8", --base (delete this next time)
+  "d64116a4-8b57-4ff4-a08c-1b373582209f",-- s3ji aka Arabic symbols hwid
   "skibidi" -- kys - keep yourself safe
 }
 -- what are you doing here lil bro
